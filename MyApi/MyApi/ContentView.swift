62a86788d4aef187cbd194366905a0eca41e82f3
cr <tscryg@hotmail.com>
2019-08-14 09:32:33 +0800

    create

---
+++ MyApi/MyApi/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MyApi
+//
+//  Created by tscryg on 2019/8/14.
+//  Copyright © 2019 tscryg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
